e5ae1beb55958683ec50292c98457e613e67b711
invad0r <invad0r@gmx.net>
2013-04-07 21:14:14 +0200

    essen.php erstellt; hier sollen die essen dargestellt werden

---
+++ essen.php
@@ -0,0 +1,37 @@
+<?php
+
+require 'config/dbconfig.php';
+require 'template.php';
+
+//Always place this code at the top of the Page
+session_start();
+if (isset($_SESSION['id'])) {
+    // Redirection to login page twitter or facebook
+    header("location: home.php");
+}
+
+if (array_key_exists("login", $_GET)) {
+    $oauth_provider = $_GET['oauth_provider'];
+    if ($oauth_provider == 'twitter') {
+        header("Location: login-twitter.php");
+    } else if ($oauth_provider == 'facebook') {
+        header("Location: login-facebook.php");
+    }
+}
+
+$content = '
+<div>Essen</div>
+				';
+
+//if(isset($database))echo "yey";
+
+if (isset ($_POST["Armgartstraße"])) echo "Armgartstraße <br/>";
+
+
+
+$content.='
+
+';
+
+echo $template_beginning.$content.$template_ending;
+?>
