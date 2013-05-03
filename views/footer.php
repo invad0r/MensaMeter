695ed53772eac90a390718f60c84ebc79fa5d9b2
invad0r <invad0r@gmx.net>
2013-05-03 22:08:41 +0200

    edited header and footer to load JQM

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,26 @@
-        </div>
-        <div id="footer">
-            (c) Footer
-        </div>
-    </body>
+</div>
+<div id="footer" data-role="footer" data-position="fixed">
+    <div data-role="navbar" data-iconpos="top" >
+        <ul>
+            <?php if (Session::get('loggedIn') == false): ?>
+                <li><a href="<?php echo URL ?>index" data-icon="grid">Index</a></li>
+
+            <?php endif; ?>
+            <li><a href="<?php echo URL ?>mensa" data-icon="home">Mensen</a></li>
+            <li><a href="<?php echo URL ?>help" data-icon="home">Help</a></li>
+            <?php if (Session::get('loggedIn') == true): ?>
+                <li><a href="<?php echo URL ?>dashboard" data-icon="star">Dashboard</a></li>
+
+                <?php if (Session::get('role') == 'owner'): ?>
+                    <li><a href="<?php echo URL ?>user" data-icon="star">User</a></li>
+                <?php endif; ?>
+
+                <li><a href="<?php echo URL ?>dashboard/logout" data-icon="star">LogOut</a></li>
+            <?php else: ?>
+                <li><a href="<?php echo URL ?>logIn" data-icon="star">Login</a></li>
+            <?php endif; ?>
+        </ul>
+    </div>
+</div>
+</body>
 </html>
--- views/header.php
+++ views/header.php
@@ -1,8 +1,14 @@
 <html> 
     <head>
-        <title>Test</title>
-        <link rel="stylesheet" href="<?php echo URL ?>public/css/default.css" />
-        <script type="text/javascript" src="<?php echo URL ?>public/js/jquery-1.9.1.js"></script>
+        <title>MensaMeter</title>
+<!--        <link rel="stylesheet" href="<?php echo URL ?>public/css/default.css" />-->
+        <link rel="stylesheet" href="<?php echo URL ?>public/css/vendor/jquery.mobile-1.3.1.min.css" />
+        <link rel="stylesheet" href="<?php echo URL ?>public/css/vendor/jquery.mobile.structure-1.3.1.min.css" />
+        <link rel="stylesheet" href="<?php echo URL ?>public/css/vendor/jquery.mobile.theme-1.3.1.min.css" />
+
+
+        <script type="text/javascript" src="<?php echo URL ?>public/js/vendor/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="<?php echo URL ?>public/js/vendor/jquery.mobile-1.3.1.min.js"></script>
         <script type="text/javascript" src="<?php echo URL ?>public/js/custom.js"></script>
         <?php
         if (isset($this->js)) { //insider the view now! that's why it isn't $this->view->js !
@@ -16,25 +22,8 @@
 
         <?php Session::init(); ?>
 
-        <div id="header">
-
-
-            <?php if (Session::get('loggedIn') == false): ?>
-                <a href="<?php echo URL ?>index">Index</a>
-                <a href="<?php echo URL ?>help">Help</a>
-            <?php endif; ?>
-
-            <?php if (Session::get('loggedIn') == true): ?>
-                <a href="<?php echo URL ?>dashboard">Dashboard</a>
-
-                <?php if (Session::get('role') == 'owner'): ?>
-                    <a href="<?php echo URL ?>user">User</a>
-                <?php endif; ?>
-
-                <a href="<?php echo URL ?>dashboard/logout">LogOut</a>
-            <?php else: ?>
-                <a href="<?php echo URL ?>logIn">Login</a>
-            <?php endif; ?>
-
+        <div id="header" data-role="header">
+            <h1>MensaMeter</h1>
         </div>
         <div id="content">
+            
