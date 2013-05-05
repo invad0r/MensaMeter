720459ee4ba3ce39b7f84e7874081c697e5ad932
invad0r <invad0r@gmx.net>
2013-05-05 05:20:02 +0200

    included  h5bp / mobile-boilerplate meta and link->images

--- views/header.php
+++ views/header.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
 <html> 
     <head>
-        <title>MensaMeter</title>
+        <meta charset="utf-8">
+        <title>Mensa Meter</title>
+        <meta name="description" content="">
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="cleartype" content="on">
+        
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="public/imges/touch/apple-touch-icon-144x144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="public/imges/touch/apple-touch-icon-114x114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="public/imges/touch/apple-touch-icon-72x72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="public/imges/touch/apple-touch-icon-57x57-precomposed.png">
+        <link rel="shortcut icon" href="img/touch/apple-touch-icon.png">
+        
 <!--        <link rel="stylesheet" href="<?php echo URL ?>public/css/default.css" />-->
         <link rel="stylesheet" href="<?php echo URL ?>public/css/vendor/jquery.mobile-1.3.1.min.css" />
         <link rel="stylesheet" href="<?php echo URL ?>public/css/vendor/jquery.mobile.structure-1.3.1.min.css" />
@@ -26,4 +40,4 @@
             <h1>MensaMeter</h1>
         </div>
         <div id="content">
-            
+
