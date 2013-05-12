7a02b07187bb1fcbeb7f3c91af9bb09b0e1aea30
invad0r <invad0r@gmx.net>
2013-05-12 19:37:39 +0200

    <meta> in header verändert
    - apple-mobile-web-app-capable hinzugefügt
    - viewport angepasst

--- views/header.php
+++ views/header.php
@@ -6,7 +6,8 @@
         <meta name="description" content="">
         <meta name="HandheldFriendly" content="True">
         <meta name="MobileOptimized" content="320">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0"> 
         <meta http-equiv="cleartype" content="on">
         
         <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo URL ?>public/imges/touch/apple-touch-icon-144x144-precomposed.png">
@@ -20,7 +21,7 @@
         <link rel="stylesheet" href="<?php echo URL ?>public/css/vendor/jquery.mobile.structure-1.3.1.min.css" />
         <link rel="stylesheet" href="<?php echo URL ?>public/css/vendor/jquery.mobile.theme-1.3.1.min.css" />
 
-
+        
         <script type="text/javascript" src="<?php echo URL ?>public/js/vendor/jquery-1.9.1.js"></script>
         <script type="text/javascript" src="<?php echo URL ?>public/js/vendor/jquery.mobile-1.3.1.min.js"></script>
         <script type="text/javascript" src="<?php echo URL ?>public/js/custom.js"></script>
