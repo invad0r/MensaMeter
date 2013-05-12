df18d08caf3a2be0f8aa7f975e9b341d9e6422cd
invad0r <invad0r@gmx.net>
2013-05-12 19:05:09 +0200

    Pfade angepasst
    <?php echo URL ?> hat bei den Bildern gefehlt

--- views/header.php
+++ views/header.php
@@ -9,11 +9,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="cleartype" content="on">
         
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="public/imges/touch/apple-touch-icon-144x144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="public/imges/touch/apple-touch-icon-114x114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="public/imges/touch/apple-touch-icon-72x72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="public/imges/touch/apple-touch-icon-57x57-precomposed.png">
-        <link rel="shortcut icon" href="img/touch/apple-touch-icon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo URL ?>public/imges/touch/apple-touch-icon-144x144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo URL ?>public/imges/touch/apple-touch-icon-114x114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo URL ?>public/imges/touch/apple-touch-icon-72x72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="<?php echo URL ?>public/imges/touch/apple-touch-icon-57x57-precomposed.png">
+        <link rel="shortcut icon" href="<?php echo URL ?>public/imges/touch/apple-touch-icon.png">
         
 <!--        <link rel="stylesheet" href="<?php echo URL ?>public/css/default.css" />-->
         <link rel="stylesheet" href="<?php echo URL ?>public/css/vendor/jquery.mobile-1.3.1.min.css" />
