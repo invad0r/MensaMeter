b3918b1f42eefa979784881bf041512da35ec1a8
lebeokute <hoangle.dam@haw-hamburg.de>
2013-06-05 09:47:54 +0200

    Essen zur Mensa

--- views/mensa/essen.php
+++ views/mensa/essen.php
@@ -2,21 +2,31 @@
 <div data-role="listview" data-inset="true">
 
     <?php
+ $q=$_POST["mensa"];
+ //echo $q;
+ //echo "abc";
     echo "<table border='1'>
 <tr>
 <th>Kategorie</th>
 <th>Speise</th>
-<th>Preis</th>
+<th>Preis Studierende</th>
+<th>Preis Bedienstete</th>
 <th>Bewertung</th>
 </tr>";
-  
-    foreach ($this->essenList as $key => $value) { 
-        
+    
+    foreach ($this->essenList as $key => $value ) { 
+       if($value['mensa_id']==$q){
         echo "<tr>";
-        echo "<td> </td>";      
-        echo "<td>" . $value['name'] . "</td>";
-        echo "</tr>";     
+        echo "<td>".$kat. "</td>";
+        echo "<td>" .$value['name']. "</td>";
+        echo "<td>".$preis_std. "</td>";
+        echo "<td>".$preis_mit. "</td>";
+        echo "<td>".$bewertung. "</td>";    
+       echo "</tr>";    
+       }
     }
+    
+    
     echo "</table>";
 
     ?>
