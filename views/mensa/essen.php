238cf78313ca71c55c70df310befa9b7fc99d5aa
lebeokute <hoangle.dam@haw-hamburg.de>
2013-07-21 17:57:29 +0200

    preis and kategorie are added

--- views/mensa/essen.php
+++ views/mensa/essen.php
@@ -3,6 +3,8 @@
 
     <?php
  $q=$_POST["mensa"];
+ mysql_connect(localhost,root);
+ mysql_select_db(danieltinney) or die ( "Unable to select database");
  //echo $q;
  //echo "abc";
     echo "<table border='1'>
@@ -13,24 +15,46 @@
 <th>Preis Bedienstete</th>
 <th>Bewertung</th>
 </tr>";
-    
-    foreach ($this->essenList as $key => $value ) { 
-       if($value['mensa_id']==$q){
-        echo "<tr>";
-        echo "<td>".$kat. "</td>";
-        echo "<td>" .$value['name']. "</td>";
-        echo "<td>".$preis_std. "</td>";
-        echo "<td>".$preis_mit. "</td>";
-        echo "<td>".$bewertung. "</td>";    
-       echo "</tr>";    
-       }
+     
+    echo "<tr>"; 
+   
+    foreach ($this->essenList as $key => $value ){
+        if($value['mensa_id']==$q){ 
+        if($value['kat_id']=='1'){
+        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '1'");}
+        if($value['kat_id']=='2'){
+        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '2'");}
+        if($value['kat_id']=='3'){
+        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '3'");}
+        if($value['kat_id']=='4'){
+        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '4'");}
+        if($value['kat_id']=='5'){
+        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '5'");}
+        if($value['kat_id']=='6'){
+        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '6'");}
+        if($value['kat_id']=='7'){
+        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '7'");}
+        if($value['kat_id']=='8'){
+        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '8'");}
+        
+            while($row=  mysql_fetch_array($result))
+            {
+            echo "<td>" .$row['name']. "</td>";
+            
+            echo "<td>" .$value['name']. "</td>";
+            
+            echo "<td>".$row['preis_std']. "</td>";
+            
+            echo "<td>".$row['preis_mit']. "</td>";
+            
+            echo "<td>".$bewertung. "</td>";   
+            }
+    echo "</tr>";    
+        }
     }
-    
-    
     echo "</table>";
 
     ?>
- 
-</div>
+ </div>
 
 
