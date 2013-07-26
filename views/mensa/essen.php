da9da5f75d6c10c2182f3bbbf307ec4775e2b281
lebeokute <hoangle.dam@haw-hamburg.de>
2013-07-26 10:01:42 +0200

    mysql verkuerzt

--- views/mensa/essen.php
+++ views/mensa/essen.php
@@ -5,8 +5,13 @@
  $q=$_POST["mensa"];
  mysql_connect(localhost,root);
  mysql_select_db(danieltinney) or die ( "Unable to select database");
- //echo $q;
- //echo "abc";
+ $array_katid=array();
+  foreach ($this->kategorieList as $key1 => $value1 )
+  {
+      array_push($array_katid,$value1['id']);
+      
+  }
+
     echo "<table border='1'>
 <tr>
 <th>Kategorie</th>
@@ -15,32 +20,23 @@
 <th>Preis Bedienstete</th>
 <th>Bewertung</th>
 </tr>";
-     
+  
     echo "<tr>"; 
    
     foreach ($this->essenList as $key => $value ){
         if($value['mensa_id']==$q){ 
-        if($value['kat_id']=='1'){
-        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '1'");}
-        if($value['kat_id']=='2'){
-        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '2'");}
-        if($value['kat_id']=='3'){
-        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '3'");}
-        if($value['kat_id']=='4'){
-        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '4'");}
-        if($value['kat_id']=='5'){
-        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '5'");}
-        if($value['kat_id']=='6'){
-        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '6'");}
-        if($value['kat_id']=='7'){
-        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '7'");}
-        if($value['kat_id']=='8'){
-        $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '8'");}
-        
-            while($row=  mysql_fetch_array($result))
-            {
-            echo "<td>" .$row['name']. "</td>";
             
+            echo $value['kat_id'];
+            $i=0;
+            
+        while ($i<=sizeof($array_katid)){
+        
+            if($value['kat_id']==$array_katid[$i]){
+            $result = mysql_query("SELECT name,preis_std,preis_mit FROM kategorie WHERE id = '$array_katid[$i]'");
+            while($row=  mysql_fetch_array($result)){
+                
+            echo "<td>" .$row['name']. "</td>";
+                 
             echo "<td>" .$value['name']. "</td>";
             
             echo "<td>".$row['preis_std']. "</td>";
@@ -48,13 +44,16 @@
             echo "<td>".$row['preis_mit']. "</td>";
             
             echo "<td>".$bewertung. "</td>";   
+           }
             }
+           $i++;   
     echo "</tr>";    
-        }
+        
+        } 
     }
+    }
+    
     echo "</table>";
 
     ?>
  </div>
-
-
