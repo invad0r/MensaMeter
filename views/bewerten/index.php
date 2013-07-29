f71f3d2513d1ae02797b53eeab138d865e34de08
lebeokute <hoangle.dam@haw-hamburg.de>
2013-07-29 23:09:22 +0200

    Bewertenseite verbessern

--- views/bewerten/index.php
+++ views/bewerten/index.php
@@ -1,11 +1,19 @@
-<?PHP
-
-session_start();
-
-if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
-
-header ("Location: login.php");
-
-}
-
-?>
+<form action="<?php echo URL; ?>bewerten" method="post">
+     <?php if (Session::get('loggedIn') == true): ?>
+       <div id="bewertung">
+           <fieldset>
+	<label for="punkt">Bewertung abgeben:</label>
+	<input type="range" name="punkt" id="punkt" value="0" min="0" max="10" data-theme="a" data-track-theme="b" />
+          
+    
+        <input type="submit" value="Absenden" />   
+     
+     <?php endif; ?>
+     <?php if (Session::get('loggedIn') == false): 
+ 
+            header ("Location: login");
+     ?>
+         <?php endif; ?>
+            </fieldset>
+           </div>
+    </form>
